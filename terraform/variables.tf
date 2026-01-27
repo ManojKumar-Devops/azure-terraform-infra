0a9d2f9d870c345c599f0f187b8fe295be0a4b04
ManojKumar <manojkumarr@redgrapebs.com>
2026-01-27 16:27:03 +0000

    "Azure-Terraform-Pipeline Structure"

---
+++ terraform/variables.tf
@@ -0,0 +1,10 @@
+variable "resource_group_name" {
+  description = "Resource group name"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+  default     = "East US"
+}
