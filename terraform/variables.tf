5daa60aa09f0188890082e2e9767e6ccee5f669e
ManojKumar <manojkumarr@redgrapebs.com>
2026-01-28 06:10:48 +0000

    "Simple Terraform Pipeline for Azure"

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,10 +1,15 @@
-variable "resource_group_name" {
-  description = "Resource group name"
-  type        = string
+variable "location" {
+  default = "East US"
 }
 
-variable "location" {
-  description = "Azure region"
-  type        = string
-  default     = "East US"
+variable "resource_group_name" {
+  default = "rg-terraform-demo"
+}
+
+variable "vm_admin_username" {
+  default = "azureuser"
+}
+
+variable "vm_size" {
+  default = "Standard_B1s"
 }
