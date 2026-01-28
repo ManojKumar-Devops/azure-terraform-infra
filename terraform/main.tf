aec9e2d0d396a3b8176903300422ce968fab35b6
ManojKumar <manojkumarr@redgrapebs.com>
2026-01-28 13:18:23 +0000

    "changed public ip"

--- terraform/main.tf
+++ terraform/main.tf
@@ -62,8 +62,11 @@ resource "azurerm_public_ip" "pip" {
   name                = "manoj-demo-pip"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Dynamic"
+
+  allocation_method = "Static"
+  sku               = "Standard"
 }
+
 
 ############################
 # Network Interface
