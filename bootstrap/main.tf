1501a43d08713825cdfdd04b3351e98214451245
ManojKumar <manojkumarr@redgrapebs.com>
2026-01-29 07:35:49 +0000

    "Added bootstrap and main.tf"

---
+++ bootstrap/main.tf
@@ -0,0 +1,58 @@
+terraform {
+  required_version = ">= 1.5.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "tfstate" {
+  name     = "tfstate-rg"
+  location = "East US"
+}
+
+resource "azurerm_storage_account" "tfstate" {
+  name                     = "tfstateazuredev123" # MUST be globally unique
+  resource_group_name      = azurerm_resource_group.tfstate.name
+  location                 = azurerm_resource_group.tfstate.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  min_tls_version          = "TLS1_2"
+  allow_blob_public_access = false
+}
+
+resource "azurerm_storage_container" "tfstate" {
+  name                  = "tfstate"
+  storage_account_name  = azurerm_storage_account.tfstate.name
+  container_access_type = "private"
+}
+terraform {
+  required_version = ">= 1.5.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "tfstate" {
+  name     = "tfstate-rg"
+  location = "East US"
+}
+
+resource "azurerm_storage_account" "tfstate" {
+  name                     = "tfstateazuredev123" # MUST be globally unique
+  resource_group_name      = azurerm_resource_group.tfstate.name
+  location                 = azurerm_resource_group.tfstate.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  min_tls_version          = "TLS1_2"
+  allow_blob_public_access = false
+}
+
+resource "azurerm_storage_container" "tfstate" {
+  name                  = "tfstate"
+  storage_account_name  = azurerm_storage_account.tfstate.name
+  container_access_type = "private"
+}
