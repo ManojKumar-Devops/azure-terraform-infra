4604db53076324b534e246925e7af9ceaa9cf46a
ManojKumar <manojkumarr@redgrapebs.com>
2026-01-29 10:40:05 +0000

    "New Terraform Deployment in free env"

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,6 +1,7 @@
 variable "environment" {
   description = "Environment name"
   type        = string
+  default     = "dev"
 }
 
 variable "location" {
@@ -8,5 +9,10 @@ variable "location" {
 }
 
 variable "resource_group_name" {
-  description = "RG name"
+  default = "azure-terraform"
+}
+
+variable "ssh_public_key" {
+  description = "Path to SSH public key"
+  default     = "~/.ssh/id_rsa.pub"
 }
